06e1357ccafd65b06332785937cd89765667ac7a
cti23gy <70219724+cti23gy@users.noreply.github.com>
2021-09-22 17:00:33 -0400

    add to public

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,9 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://in-info-web4.informatics.iupui.edu/~ctimel/N322/to-do-list",
     title: "ToDo-List",
   },
   plugins: ["gatsby-plugin-gatsby-cloud"],
+  pathPrefix: '/~ctimel/N322/to-do-list'
+
 };
